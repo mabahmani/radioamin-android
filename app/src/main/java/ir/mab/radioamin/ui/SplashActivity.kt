cfdb0324b2b78e0e9034b492037a3ce6cfd293c7
Mohammad Amin Bahmani <aminbahmani10@gmail.com>
2021-06-17 23:09:41 +0430

    add splash screen

---
+++ app/src/main/java/ir/mab/radioamin/ui/SplashActivity.kt
@@ -0,0 +1,12 @@
+package ir.mab.radioamin.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ir.mab.radioamin.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
