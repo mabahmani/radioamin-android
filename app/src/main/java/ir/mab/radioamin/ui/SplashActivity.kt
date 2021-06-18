d993ac7f46fba07d8083984f5547f511f3eb4452
Mohammad Amin Bahmani <aminbahmani10@gmail.com>
2021-06-18 15:20:16 +0430

    impl splash screen

--- app/src/main/java/ir/mab/radioamin/ui/SplashActivity.kt
+++ app/src/main/java/ir/mab/radioamin/ui/SplashActivity.kt
@@ -6,6 +6,7 @@ import ir.mab.radioamin.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
     }
