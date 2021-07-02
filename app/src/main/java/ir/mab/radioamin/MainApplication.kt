c39051fcd6f33ae9a170d110c8df2aa7a17ff2e2
Mohammad Amin Bahmani <aminbahmani10@gmail.com>
2021-07-02 16:30:52 +0430

    impl dependency injection fundamental classes

--- app/src/main/java/ir/mab/radioamin/MainApplication.kt
+++ app/src/main/java/ir/mab/radioamin/MainApplication.kt
@@ -1,10 +1,12 @@
 package ir.mab.radioamin
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import io.github.inflationx.calligraphy3.CalligraphyConfig
 import io.github.inflationx.calligraphy3.CalligraphyInterceptor
 import io.github.inflationx.viewpump.ViewPump
 
+@HiltAndroidApp
 class MainApplication: Application() {
 
     override fun onCreate() {
