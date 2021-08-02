9a57d5654d7836cfc0dd4627827da64f5593011f
Mohammad Amin Bahmani <aminbahmani10@gmail.com>
2021-08-02 23:14:48 +0430

    impl change background color attr for ProgressCompoundView.kt

--- app/src/main/java/ir/mab/radioamin/ui/custom/ProgressCompoundView.kt
+++ app/src/main/java/ir/mab/radioamin/ui/custom/ProgressCompoundView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.widget.FrameLayout
 import android.widget.RelativeLayout
+import androidx.core.content.ContextCompat
 import ir.mab.radioamin.R
 
 class ProgressCompoundView @JvmOverloads constructor(
@@ -12,21 +13,39 @@ class ProgressCompoundView @JvmOverloads constructor(
 
     init {
         inflate(context, R.layout.custom_progress_view, this)
+
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.ProgressCompoundView,
+            0, 0
+        ).apply {
+
+            try {
+                findViewById<RelativeLayout>(R.id.parent).setBackgroundColor(
+                    getColor(
+                        R.styleable.ProgressCompoundView_prBackgroundColor,
+                        ContextCompat.getColor(context, R.color.color1)
+                    )
+                )
+            } finally {
+                recycle()
+            }
+        }
     }
 
     fun setVisibilityWithAnim(visibility: Int) {
         val view = findViewById<RelativeLayout>(R.id.parent)
         when (visibility) {
             VISIBLE -> {
-                view.animate().alpha(1f).setDuration(300)
+                view.animate().alpha(1f).setDuration(200)
                     .withStartAction { view.visibility = VISIBLE }.start()
             }
             GONE -> {
-                view.animate().alpha(0f).setDuration(300)
+                view.animate().alpha(0f).setDuration(200)
                     .withEndAction { view.visibility = GONE }.start()
             }
             INVISIBLE -> {
-                view.animate().alpha(0f).setDuration(300)
+                view.animate().alpha(0f).setDuration(200)
                     .withEndAction { view.visibility = INVISIBLE }.start()
             }
         }
